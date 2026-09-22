b320d063dd9107c5d15f1e3b6fa2284bcd0a44b4
Gabriel <gabriel.leandrods@gmail.com>
2026-09-22 20:46:07 -0300

    feat(s05): adiciona endpoint de saudação com nome e contador

---
+++ src/WebApiLab.Api/Model/Greeting.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiLab.Api.Model
+{
+    public record Greeting(long Id, string content);
+}
